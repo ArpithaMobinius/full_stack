274a4d9b6638c76183a6d8935b0edc09b5c756f6
Arpitha Gowda <arpitha.hn@mobinius.com>
2019-12-31 21:56:06 +0530

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react'
 
+const DisplayNumbers = (props) => {
+  let filtered = props.persons.filter(person => person.name.match(new RegExp(props.filterName, 'i')))
+
+  return filtered.map((persons, index) => {
+    return (
+      <p key={index}> {persons.name} {persons.numbers}</p>      
+    )
+  })
+}
+
 const App = () => {
-  const [ persons, setPersons] = useState([])
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState(0)
+  const [ filterName, setfilterName ] = useState('')
 
   const mySubmitHandler = (event) => {
     event.preventDefault();
@@ -30,9 +46,17 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const filterChangeHandler = (event) => {
+    setfilterName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input name='filter' onChange={filterChangeHandler}/>
+      </div>
+      <h3>Add a new</h3>
       <form onSubmit={mySubmitHandler}>
         <div>
           name: <input name='name' onChange={nameChangeHandler}/>
@@ -43,7 +67,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((persons, index) => <div key={index}>{persons.name} {persons.number}</div>)}
+      <DisplayNumbers persons={persons} filterName={filterName}/>
     </div>
   )
 }
